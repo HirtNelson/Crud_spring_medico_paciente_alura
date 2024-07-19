84da17e6317d668ba7e3a06251c8b7ebf6c6ab80
Nelson Hirt <nelsonhirt827@gmail.com>
2024-07-18 23:08:57 -0300

    Atualizando

---
+++ src/main/java/med/voll/api/domain/consulta/AgendaDeConsultas.java
@@ -0,0 +1,57 @@
+package med.voll.api.domain.consulta;
+
+import med.voll.api.domain.ValidacaoException;
+import med.voll.api.domain.medico.Medico;
+import med.voll.api.domain.medico.MedicoRepository;
+import med.voll.api.domain.paciente.PacienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AgendaDeConsultas {
+
+    @Autowired
+    private ConsultaRepository consultaRepository;
+    @Autowired
+    private PacienteRepository pacienteRepository;
+    @Autowired
+    private MedicoRepository medicoRepository;
+
+    public void agendar(DadosAgendamentoConsulta dados){
+
+        if (!pacienteRepository.existsById(dados.idPaciente())) {
+            throw new ValidacaoException("Paciente informado não existe.");
+        }
+
+        if (dados.idMedico() != null && !medicoRepository.existsById(dados.idMedico())){
+            throw new ValidacaoException("Médico informado não existe.");
+        }
+
+        var medico = escolherMedico(dados);
+        var paciente = pacienteRepository.getReferenceById(dados.idPaciente());
+        var consulta = new Consulta(null, paciente, medico, dados.data(), null);
+        consultaRepository.save(consulta);
+
+    }
+
+    private Medico escolherMedico(DadosAgendamentoConsulta dados) {
+        if (dados.idMedico() != null){
+            return medicoRepository.getReferenceById(dados.idMedico());
+        }
+        if (dados.especialidade() == null){
+            throw new ValidacaoException("Especialidade obrigatoria quando não escolher médico.");
+        }
+
+        return medicoRepository.escolherMedicoAleatorioLivreNaData(dados.especialidade(),dados.data());
+
+
+    }
+
+    public void cancelarConsulta(DadosCancelamentoDaConsulta dados) {
+        if (!consultaRepository.existsById(dados.id_Consulta())){
+            throw new ValidacaoException("Consulta não está agendada ou não existe.");
+        }
+        var consulta = consultaRepository.getReferenceById(dados.id_Consulta());
+        consulta.cancelar(dados.motivo());
+    }
+}
---
+++ src/main/java/med/voll/api/domain/consulta/validacoes/ValidadorMedicoComConsultaNoMesmoHorario.java
@@ -0,0 +1,18 @@
+package med.voll.api.domain.consulta.validacoes;
+
+import med.voll.api.domain.ValidacaoException;
+import med.voll.api.domain.consulta.ConsultaRepository;
+import med.voll.api.domain.consulta.DadosAgendamentoConsulta;
+
+public class ValidadorMedicoComConsultaNoMesmoHorario {
+
+    private ConsultaRepository consultaRepository;
+
+    public void validar(DadosAgendamentoConsulta dados){
+
+        var medicoPossuiOutraConsultaNoMesmoHorario = consultaRepository.existsByMedicoIdAndData(dados.idMedico(), dados.data());
+        if (medicoPossuiOutraConsultaNoMesmoHorario){
+            throw new ValidacaoException("Medico indisponivel neste horário.");
+        }
+    }
+}
